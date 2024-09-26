c435bf7a010dbc1724c39c5956434f06fe9c2d74
Felipe Marinho <github@felipemarinho.dev>
2024-09-26 15:15:19 -0700

    m01.3.2 - install `nodemon`

--- apps/company-server/index.js
+++ apps/company-server/index.js
@@ -3,6 +3,6 @@ const http = require("http");
 console.log('Node JS!');
 
 http.createServer(async (req, res) => {
-  res.write(JSON.stringify({ message: "Hello from Node Server!"}));
+  res.write(JSON.stringify({ message: "Hello from Node Server!!!" }));
   res.end();
 }).listen(4000);
