d827d70300015d8f02090f1de17af6a538b82f5d
Felipe Marinho <github@felipemarinho.dev>
2024-09-25 23:08:35 -0700

    m01.3.1 - create `http-client` lib

--- apps/company-front/app/page.tsx
+++ apps/company-front/app/page.tsx
@@ -1,3 +1,11 @@
+"use client"
+
+import { httpClient } from "@felipemarinhodev/commons-http-client";
+
 export default function Page () {
+
+  httpClient.get("https://api.github.com/users/felipemarinhodev")
+    .then((data) => console.log(data));
+
   return <h1>Company front</h1>;
 }
