85b0f5e6f044e8811885adad2ee7cbd1b4aa5126
Felipe Marinho <github@felipemarinho.dev>
2024-09-29 18:27:45 -0700

    a3.3.2 - update script to run when a package change

--- _scripts/run-on-changed-packages.ts
+++ _scripts/run-on-changed-packages.ts
@@ -1,16 +1,38 @@
 import { execSync } from "child_process";
+import fs from "fs";
 
 const diffChangedFiles = execSync("git diff --name-only HEAD^..HEAD", {
   encoding: "utf-8"
 }).split("\n");
-console.log("diffChangedFiles", diffChangedFiles);
 
-// TODO: como mapear o `diffChangedFiles` para `changedPackageNames`
+const changedPackageNames = diffChangedFiles
+  // 1 - Precisamos ter os paths dos package JSONs
+  .map((path) => {
+    const [workspace, packageFolder] = path.split("/");
+    if (workspace !== "apps" && workspace !== "commons") return null;
+    return `${workspace}/${packageFolder}/package.json`;
+  })
+  // 1.1 - Remover os valores `nulls`
+  .filter(Boolean)
+  // 2 - Podemos ter algum duplicado, precismos remover os duplicados
+  .filter((pathToPackageJSON, index, arr) => {
+    return arr.indexOf(pathToPackageJSON) === index;
+  })
+  // 3 - Ler os arquivos `package.json` e pegar o nome dentro do package JSON 
+  .reduce((_changePackageNames: string[], pathToPackageJSON: string | null) => {
+    if (pathToPackageJSON) {
+      const packageJSON = JSON.parse(fs.readFileSync(pathToPackageJSON, {
+        encoding: "utf-8"
+      }));
+      const packageName = packageJSON.name;
+      return [
+        ..._changePackageNames,
+        packageName
+      ]
+    }
+return _changePackageNames;
+  }, [] as string[]);
 
-const changedPackageNames = [
-  // "company-front",
-  // "company-server"
-]
 const workspacesFlags = changedPackageNames.map((packageName) => {
   return `--workspace=${packageName}`}).join(" ");
 
